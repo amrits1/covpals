c0702b9c25b2ecb442898b00868d5c1f08b608f4
amrits1 <AmritGithub@gmail.com>
2021-02-20 15:25:57 -0600

    Fixed problem with boolean values

--- server/routes/api/endpoints.js
+++ server/routes/api/endpoints.js
@@ -4,44 +4,44 @@ const Users = require('../../models/schema');
 
 router.get('/test', async (req, res) => {
     let user = {
-        name: "test",
+        name: "george",
         email: "testemail",
         zoom: "",
         matched: false,
         partner: {name: "a", time: {day: 0, hour: 0}, email: "d"},
-        availability: {sunday: [], monday: [0], tuesday: [1, 4, 6, 7], wednesday: [], thursday: [23], friday: [], saturday: [1]}};
+        availability: {sunday: [], monday: [], tuesday: [7], wednesday: [], thursday: [21], friday: [], saturday: []}};
     await Users.create(user);
 }) //testing
 
 // change to post after
 router.get('/create', async (req, res) => {
     let user = {
-        name: "test",
-        email: "testemail",
+        name: "harold3",
+        email: "testemail2",
         zoom: "",
         matched: false,
         partner: {name: "a", time: {day: 0, hour: 0}, email: "d"},
-        availability: {sunday: [], monday: [0], tuesday: [1, 4, 6, 7], wednesday: [], thursday: [23], friday: [], saturday: []}
+        availability: {sunday: [], monday: [], tuesday: [7], wednesday: [3], thursday: [23], friday: [6], saturday: [1]}
         //name: req.body.name,
         //email: req.body.email,
         //availability: req.body.availability
         //zoom: "",
         //matched: false,
-        //partner: {name: "", time: {day: 0, hour: 0}, email: ""},
+        //partner: {name: "", time: {day: 0, hour: 0}, email: ""}
     };
 
-    Users.findOne({ $or: [
+    Users.findOne({ $and: [ { $or: [
         { "availability.monday": { $elemMatch: { $in: user.availability.monday} } },
         { "availability.tuesday": { $elemMatch: { $in: user.availability.tuesday} } },
         { "availability.wednesday": { $elemMatch: { $in: user.availability.wednesday} } },
         { "availability.thursday": { $elemMatch: { $in: user.availability.thursday} } },
         { "availability.friday": { $elemMatch: { $in: user.availability.friday} } }
-    ] }, async function (err, result) {
+    ] }, {matched: false} ] }, async function (err, result) {
         if (err) {
-            return handleError(err);
+            return;
         } 
         else if (result==null) {
-            user.partner = {name: "", email: "", time: {day: 0, hour: 0}};
+            user.partner = {name: "", email: "", time: {day: -1, hour: -1}};
             user.zoom = "";
             user.matched = false;
             await Users.create(user);
@@ -49,14 +49,19 @@ router.get('/create', async (req, res) => {
         else {
             for (day in user.availability) {
                 let common = user.availability[day].filter(x => result.availability[day].includes(x));
-                if (common) {
+                if (common.length != 0) {
                     dayInt = ["sunday","monday","tuesday","wednesday","thursday","friday","saturday"].indexOf(day);
                     user.partner = {name: result.name, email: result.email, time: {day: dayInt, hour: common[0]}};
                     user.zoom = ""; // placeholder for creating the link
                     user.matched = true;
                     await Users.create(user);
-
+                    
                     // need to also updating existing result
+                    result.partner = {name: user.name, email: user.name, time: {day: dayInt, hour: common[0]}};
+                    result.zoom = ""; // placeholder for creating the link
+                    result.matched = true;
+                    await Users.replaceOne( {_id: result._id}, result);
+                    
                     break;
                 }
             }
