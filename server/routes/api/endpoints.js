6307f2e9113f5c442091bdfe016ed0f5bd2c37b4
amrits1 <AmritGithub@gmail.com>
2021-02-20 21:54:21 -0600

    Implemented system to generate correct meeting start times

--- server/routes/api/endpoints.js
+++ server/routes/api/endpoints.js
@@ -4,6 +4,7 @@ const Users = require('../../models/schema');
 const axios = require('axios');
 require('dotenv').config();
 const zoomKey = process.env.ZOOM_KEY;
+const email = process.env.EMAIL;
 
 // post request
 router.post('/create', async (req, res) => {
@@ -38,11 +39,24 @@ router.post('/create', async (req, res) => {
             (async () => {for (day in user.availability) {
                 let common = user.availability[day].filter(x => result.availability[day].includes(x));
                 if (common.length != 0) {
-                    dayInt = ["sunday","monday","tuesday","wednesday","thursday","friday","saturday"].indexOf(day);
-                    let params = {topic:"Meeting someone new!", recurrence:{type: 2, repeat_interval: 1, weekly_days: (dayInt + 1) }, settings: {jbh_time: 10, registration_type: 1, join_before_host: true}}
+                    let dayInt = ["sunday","monday","tuesday","wednesday","thursday","friday","saturday"].indexOf(day);
+
+                    function nextDay(d, dow){
+                        d.setUTCDate(d.getUTCDate() + (dow+(7-d.getUTCDay())) % 7);
+                        return d;
+                    }
+
+                    let meetingDay = nextDay(new Date(), dayInt);
+                    meetingDay.setUTCHours(common[0]);
+                    meetingDay.setUTCMinutes(0);
+                    meetingDay.setUTCSeconds(0);
+
+                    startTime = meetingDay.toISOString().split('.').shift() + 'Z';
+
+                    let params = {topic:"Meeting someone new!", duration: 60, start_time: startTime, recurrence:{type: 2, repeat_interval: 1, weekly_days: (dayInt + 1) }, settings: {jbh_time: 10, registration_type: 1, join_before_host: true}}
 
                     let myPromise = new Promise((resolve, reject) => {
-                        axios.post('https://api.zoom.us/v2/users/a38sehga@uwaterloo.ca/meetings', params, {headers:{"content-type": "application/json", authorization: `Bearer ${zoomKey}`}}).then(response => {resolve(response.data)}).catch(response => console.log(response))
+                        axios.post('https://api.zoom.us/v2/users/'+email+'/meetings', params, {headers:{"content-type": "application/json", authorization: `Bearer ${zoomKey}`}}).then(response => {resolve(response.data)}).catch(response => console.log(response))
                     })
                     myPromise.then(async (x) => {
                         let link = x.join_url;
@@ -60,7 +74,7 @@ router.post('/create', async (req, res) => {
                         await Users.replaceOne( {_id: result._id}, result);
                         
                         // should also send an email to user.email and result.email right here notifying that they were matched
-    
+
                     })
                     break;
                 }
